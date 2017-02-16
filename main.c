bc8ba63840d10e40d474014a503545ff72d3433d
Плотников Александр Владимирович <ip613s17@mwp05.csc.local>
2017-02-16 08:27:20 +0700

    input variables

--- main.c
+++ main.c
@@ -4,6 +4,11 @@ int main()
 {
   int a;
   long int b;
+
+  printf("Enter the deposit amount: ");
+  scanf("%li",&b);
+  printf("Enter the number days less then 365");
+  scanf("%d",&a);
   
   return (0);
 }
