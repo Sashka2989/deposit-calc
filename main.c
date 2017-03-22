05a074c80f8791bcec1f0c293a1ac6972fc743ec
Alexandr Plotnikov <sashu998@gmailcom>
2017-03-22 22:03:50 +0700

    Calculate the amount of more than 100000

--- main.c
+++ main.c
@@ -6,42 +6,55 @@ void CheckSum (int n, int m);
 
 int main()
 {
-  int a;
-  long int b;
+    int a;
+    long int b;
 
-  printf("Enter the deposit amount: ");
-  scanf("%li",&b);
-  printf("Enter the number days less then 365: ");
-  scanf("%d",&a);
-  
-  CheckInp (b,a);
-  return (0);
+    printf("Enter the deposit amount: ");
+    scanf("%li",&b);
+    printf("Enter the number days less then 365: ");
+    scanf("%d",&a);
+    
+    CheckInp (b,a);
+    return (0);
 }
 
 void CheckInp (int n, int m)
 {
-  if (n<10000) {
-    printf("Deposit amount is incorrected\n");
-  }
-  if (m<0 || m>365) {
-    printf ("number days is incorrected\n");
-  }
+    if (n<10000) {
+        printf("Deposit amount is incorrected\n");
+    }
+    if (m<0 || m>365) {
+        printf ("number days is incorrected\n");
+    }
 }
 
 void CheckSum (int n, int m)
 {
-if (n < 100000) {
-		if (m >= 0 && m <= 30) {
-			n = n - (n * 10 / 100);
-		}
-		if (m >= 31 && m <= 120) {
-			n = n + (n * 2 / 100);
-		}
-		if (m >= 121 && m < 240) {
-			n = n + (n * 6 / 100);
-		}
-		if (m >= 241 && m <= 365) {
-			n = n + (n * 12 / 100);
-		}
-	}
+    if (n < 100000) {
+        if (m >= 0 && m <= 30) {
+	    n = n - (n * 10 / 100);
+        }
+        if (m >= 31 && m <= 120) {
+	    n = n + (n * 2 / 100);
+        }
+        if (m >= 121 && m < 240) {
+            n = n + (n * 6 / 100);
+        }
+        if (m >= 241 && m <= 365) {
+            n = n + (n * 12 / 100);
+        }
+    }else {
+        if (m >= 0 && m <= 30) {
+            n = n - (n * 10 / 100);
+        }
+        if (m >= 31 && m <= 120) {
+            n = n + (n * 3 / 100);
+        }
+        if (m >= 121 && m <= 240) {
+            n = n + (n * 8 / 100);
+        }
+        if (m >= 241 && m <= 365) {
+            n = n + (n * 15 / 100);
+        }
+    } 
 }
